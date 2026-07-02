bb1c1a273099ba433315fd5cb7bc21ec10d382ca
PizzaWizzas <csoulje10@gmail.com>
2026-07-01 23:29:33 -0500

    Add update.py

---
+++ update.py
@@ -0,0 +1,39 @@
+import json
+import urllib.request
+
+USER_ID = 713691
+
+query = """
+query ($id: Int) {
+  User(id: $id) {
+    statistics {
+      anime {
+        count
+        episodesWatched
+        minutesWatched
+        meanScore
+      }
+    }
+  }
+}
+"""
+
+variables = {
+    "id": USER_ID
+}
+
+data = json.dumps({
+    "query": query,
+    "variables": variables
+}).encode("utf-8")
+
+request = urllib.request.Request(
+    "https://graphql.anilist.co",
+    data=data,
+    headers={"Content-Type": "application/json"}
+)
+
+with urllib.request.urlopen(request) as response:
+    result = json.loads(response.read().decode())
+
+print(result)
